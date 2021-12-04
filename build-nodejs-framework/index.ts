a289f7640ebfe1c7c23c4ca5686d7f276a5c2cb3
dpranjic <darkopranjic.4@gmail.com>
2021-12-04 13:08:08 +0100

    Add more routes

--- build-nodejs-framework/index.ts
+++ build-nodejs-framework/index.ts
@@ -21,6 +21,26 @@ app.get("/", (_req, res) => {
   res.end("hello world");
 });
 
+app.get("/seloneveselo/:id", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("hello world from " + JSON.stringify(req.params));
+});
+
+app.get("/products/:testid", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("hello world from " + JSON.stringify(req.params));
+});
+
+app.get("/products/:testid", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("hello world from " + JSON.stringify(req.params));
+});
+
+app.get("/products/:id/student/:studentId", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("hello world from " + JSON.stringify(req.params));
+});
+
 app.listen(2000, (port) => {
   console.log(
     `Listening on adress http://${networkInterfaces.eth0[0].address}:${port}`
