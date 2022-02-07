09355656702ea362a2f6c5678397f8248d3135cd
Darko Pranjic <darkopranjic.4@gmail.com>
2022-02-07 12:53:41 +0100

    fix - React auth0 to server request

--- security/react-auth0-login/src/App.js
+++ security/react-auth0-login/src/App.js
@@ -1,17 +1,74 @@
-import logo from "./logo.svg";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 
-import { useAuth0 } from "@auth0/auth0-react";
+const GetData = () => {
+  const { getAccessTokenSilently } = useAuth0();
 
-const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const [token, setToken] = useState();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  const getToken = useCallback(async () => {
+    const retreivedToken = await getAccessTokenSilently();
+    console.log("first");
+    setToken(retreivedToken);
+  }, [getAccessTokenSilently]);
+
+  useEffect(() => {
+    getToken();
+  }, [getToken]);
+
+  console.log(token);
+
+  const getData = () => {
+    fetch("http://localhost:4000/authorized", {
+      method: "GET",
+      mode: "cors", // no-cors, *cors, same-origin
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((r) => console.log(r))
+      .catch(console.log);
+  };
+
+  const callSecureApi = async () => {
+    try {
+      const token = await getAccessTokenSilently();
+
+      const response = await fetch(`http://localhost:4000/authorized`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const responseData = await response.text();
+      console.log(responseData);
+      alert(responseData);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  return (
+    <>
+      <button onClick={callSecureApi}>Get data</button>
+      <button onClick={getToken}>Get token</button>
+    </>
+  );
 };
 
 function App() {
-  const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } =
-    useAuth0();
+  const {
+    isLoading,
+    isAuthenticated,
+    error,
+    user,
+    loginWithRedirect,
+    logout,
+    getAccessTokenSilently,
+  } = useAuth0();
+
+  console.log("user", user);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -27,6 +84,7 @@ function App() {
         <button onClick={() => logout({ returnTo: window.location.origin })}>
           Log out
         </button>
+        <GetData />
       </div>
     );
   } else {
--- security/react-auth0-login/src/index.js
+++ security/react-auth0-login/src/index.js
@@ -9,6 +9,7 @@ ReactDOM.render(
       domain={process.env.REACT_APP_AUTH0_DOMAIN}
       clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
       redirectUri={window.location.origin}
+      audience={process.env.REACT_APP_API_AUDIENCE}
     >
       <App />
     </Auth0Provider>
