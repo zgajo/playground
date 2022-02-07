fbe75deb26bab836891d8b72acea459361c6406b
Darko Pranjic <darkopranjic.4@gmail.com>
2022-02-07 17:25:00 +0100

    setup docker compose to run the nginx load balancer

--- nginx-node-postgres/src/app.ts
+++ nginx-node-postgres/src/app.ts
@@ -1,3 +1,4 @@
+require("dotenv").config();
 import express from "express";
 import { createConnection } from "./db";
 import { recipeRouter } from "./routes/recipe";
@@ -6,12 +7,18 @@ import { userRouter } from "./routes/user";
 const app = express();
 const port = 3000;
 
+console.log(process.env.APP_NO);
+
 const startApp = async () => {
   await createConnection();
 
   app.use(userRouter);
   app.use(recipeRouter);
   app.use(reviewRouter);
+
+  app.get("/", (req, res) => {
+    res.send(`Hello from world ${process.env.APP_NO || "localhost"}`);
+  });
 
   app.listen(port, () => {
     console.log(`Running on http://localhost:${port}.`);
