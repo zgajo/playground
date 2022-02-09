939bf5eb796bd631b86b04869ccd275fd86309e8
Darko Pranjic <darkopranjic.4@gmail.com>
2022-02-09 15:58:13 +0100

    Create multiple seuqelize connections

--- nginx-node-postgres/src/db/config.ts
+++ nginx-node-postgres/src/db/config.ts
@@ -15,6 +15,34 @@ if (isTest) {
 }
 
 export const sequelizeConnection = new DBConnector(dbConfig);
+
+export const insertOnlyConnection = new DBConnector({
+  username: "insert_only_user",
+  password: "Test1234",
+  database: "test",
+  host: process.env.POSTGRES_PRODUCTION_HOST || "127.0.0.1",
+  dialect: "postgres",
+  port: process.env.POSTGRES_PRODUCTION_PORT || 5433,
+});
+
+export const deleteOnlyConnection = new DBConnector({
+  username: "delete_only_user",
+  password: "Test1234",
+  database: "test",
+  host: process.env.POSTGRES_PRODUCTION_HOST || "127.0.0.1",
+  dialect: "postgres",
+  port: process.env.POSTGRES_PRODUCTION_PORT || 5433,
+});
+
+export const updateOnlyConnection = new DBConnector({
+  username: "update_only_user",
+  password: "Test1234",
+  database: "test",
+  host: process.env.POSTGRES_PRODUCTION_HOST || "127.0.0.1",
+  dialect: "postgres",
+  port: process.env.POSTGRES_PRODUCTION_PORT || 5433,
+});
+
 export const readOnlyConnection = new DBConnector({
   username: "read_only_user",
   password: "Test1234",
@@ -31,6 +59,9 @@ export const createConnections = async () => {
     try {
       await sequelizeConnection.connection.authenticate();
       await readOnlyConnection.connection.authenticate();
+      await updateOnlyConnection.connection.authenticate();
+      await deleteOnlyConnection.connection.authenticate();
+      await insertOnlyConnection.connection.authenticate();
 
       console.log("DB connection established");
       break;
