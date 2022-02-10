bdb0bb748af3111ec4d9c25e763cc0559b727b54
Darko Pranjic <darkopranjic.4@gmail.com>
2022-02-10 10:51:37 +0100

    user cursor pagination and get 1 user

--- nginx-node-postgres/src/controllers/user.ts
+++ nginx-node-postgres/src/controllers/user.ts
@@ -2,7 +2,11 @@ import { Request, Response } from "express";
 import * as userService from "../services/user";
 
 export const getUser = async function (req: Request, res: Response) {
-  const id = Number(req.params.id);
+  const id = req.params.id ? Number(req.params.id) : undefined;
+
+  if (!id) {
+    throw new Error("No id provided");
+  }
 
   const user = await userService.getById(id);
 
@@ -10,8 +14,8 @@ export const getUser = async function (req: Request, res: Response) {
 };
 
 export const listUsers = async function (req: Request, res: Response) {
-  const cursor = Number(req.query.cursor);
-  const limit = Number(req.query.limit);
+  const cursor = req.query.cursor ? Number(req.query.cursor) : undefined;
+  const limit = req.query.limit ? Number(req.query.limit) : undefined;
 
   const user = await userService.getAll({ limit, cursor });
 
