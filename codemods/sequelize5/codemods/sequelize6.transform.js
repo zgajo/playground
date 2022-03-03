ae487cf3ad9a24eb8eaaa49ae1d607a48f4d6381
Darko Pranjic <darkopranjic.4@gmail.com>
2022-03-03 11:11:55 +0100

    Track sequelize usage passed into the function

--- codemods/sequelize5/codemods/sequelize6.transform.js
+++ codemods/sequelize5/codemods/sequelize6.transform.js
@@ -6,55 +6,48 @@ const { isDbImport } = require("./helpers/sequelize");
 const isSearchedObjectName = (memberExpression, objName, objectLevel) => {
   let obj = memberExpression.object;
   while (obj) {
-    obj = obj.callee?.object;
-
     if (_.get(obj, objectLevel) === objName) {
       return true;
     }
+    obj = obj.callee?.object;
   }
 };
 
-const spreadFnChange =
-  (root, objectNameLevel) =>
-  ({ node }) => {
-    const searchName = node.id.name;
-    // find all the spread expressions if they start with imported /models etc name.
-    // e.g for imported const db = require(./models)
-    // find db.Model.find().spread
-    root
-      .find(j.Identifier, (obj) => {
-        return obj.name === "spread";
-      })
-      .forEach((spreadPath) => {
-        const { node: parent } = spreadPath.parent;
+const spreadFnChange = (root, objectNameLevel, searchName) =>
+  // find all the spread expressions if they start with imported /models etc name.
+  // e.g for imported const db = require(./models)
+  // find db.Model.find().spread
+  root
+    .find(j.Identifier, (obj) => {
+      return obj.name === "spread";
+    })
+    .forEach((spreadPath) => {
+      const { node: parent } = spreadPath.parent;
 
-        // find spread that is used by found imports
-        if (isSearchedObjectName(parent, searchName, objectNameLevel)) {
-          spreadPath.value.name = "then";
+      // find spread that is used by found imports
+      if (isSearchedObjectName(parent, searchName, objectNameLevel)) {
+        spreadPath.value.name = "then";
 
-          const spreadFunction =
-            spreadPath.parentPath?.parentPath?.value?.arguments[0];
+        const spreadFunction =
+          spreadPath.parentPath?.parentPath?.value?.arguments[0];
 
-          if (spreadFunction) {
-            // destructure parameters in stream (row)=>{} ([row])=>{}
-            if (
-              spreadFunction.type === "FunctionExpression" ||
-              spreadFunction.type === "ArrowFunctionExpression"
-            ) {
-              const spreadFunctionParams = spreadFunction.params.map(
-                (param) => {
-                  return j.identifier(param.name);
-                }
-              );
+        if (spreadFunction) {
+          // destructure parameters in stream (row)=>{} ([row])=>{}
+          if (
+            spreadFunction.type === "FunctionExpression" ||
+            spreadFunction.type === "ArrowFunctionExpression"
+          ) {
+            const spreadFunctionParams = spreadFunction.params.map((param) => {
+              return j.identifier(param.name);
+            });
 
-              const arr = j.arrayPattern(spreadFunctionParams);
+            const arr = j.arrayPattern(spreadFunctionParams);
 
-              spreadFunction.params = [arr];
-            }
+            spreadFunction.params = [arr];
           }
         }
-      });
-  };
+      }
+    });
 
 module.exports = (fileInfo, api, options) => {
   const root = j(fileInfo.source);
@@ -74,7 +67,10 @@ module.exports = (fileInfo, api, options) => {
 
       return nodePath;
     })
-    .forEach(spreadFnChange(root, "object.name"));
+    .forEach(({ node }) => {
+      const searchName = node.id.name;
+      spreadFnChange(root, "object.name", searchName);
+    });
 
   root
     .find(j.VariableDeclarator, (nodePath) => {
@@ -82,7 +78,10 @@ module.exports = (fileInfo, api, options) => {
       // for something extra we could check the passed function parameters but currently this is complicating things
       return nodePath.init?.callee?.property?.name === "define";
     })
-    .forEach(spreadFnChange(root, "name"));
+    .forEach(({ node }) => {
+      const searchName = node.id.name;
+      spreadFnChange(root, "name", searchName);
+    });
 
   // fetch all the require("sequelize") imports
   root
@@ -139,6 +138,78 @@ module.exports = (fileInfo, api, options) => {
 
               return nodePath;
             });
+        });
+    });
+
+  // search for the sequelize import, with assumption that .define is only used as sequelize method
+  root
+    .find(j.MemberExpression, (obj) => obj.property.name === "define")
+    .forEach((nodePath) => {
+      const { node } = nodePath;
+
+      const sequelizeName = node.object.name;
+
+      const foundArgumentsIndex = [];
+
+      root
+        .find(j.CallExpression, (callFn) => {
+          const index = callFn.arguments.findIndex(
+            (identifier) => identifier.name === sequelizeName
+          );
+
+          if (index === -1) return false;
+          console.log(index);
+
+          foundArgumentsIndex.push(index);
+
+          return true;
+        })
+        .forEach(({ node: callFn }, index) => {
+          // if sequelize if passed into method. e.g. kycService.getRealKycStatus(user, sequelize)
+          if (callFn.callee.object && callFn.callee.property) {
+          }
+          // if sequelize if passed into function. e.g. getRealKycStatus(user, sequelize)
+          else if (callFn.callee.name) {
+            /* check if the function is created inside of the currently checked file */
+            // Check: const getRealKycStatus = ()=>{}
+            const variableDeclarator = root.find(
+              j.VariableDeclarator,
+              (declarator) =>
+                declarator.id.name === callFn.callee.name &&
+                (declarator.init.type === "ArrowFunctionExpression" ||
+                  declarator.init.type === "FunctionExpression")
+            );
+
+            if (variableDeclarator.length) {
+              // work with this
+              const sequelizeIndex = foundArgumentsIndex[index];
+
+              const sequelizeParamName =
+                variableDeclarator.get("init")?.value?.params[sequelizeIndex]
+                  ?.name;
+
+              if (sequelizeParamName) {
+                spreadFnChange(variableDeclarator, "name", sequelizeParamName);
+              }
+
+              return;
+            }
+
+            // Check: const function getRealKycStatus (test){}
+            const functionDeclaration = root
+              .find(
+                j.FunctionDeclaration,
+                (declarator) => declarator.id.name === sequelizeName
+              )
+              .forEach(({ node: fn }) => {
+                fn.params;
+              });
+
+            /// check if the sequelize is passed into some other function
+            /// if  node then find the model and methods usage in the function
+            /* if not, then get require path from which is the fucntion imported */
+            /// create the AST tree from that file and find the function export and the function declaration
+          }
         });
     });
 
