c5674c1c4baaadde6afff51561779d6cc86bdb0e
Darko Pranjic <darkopranjic.4@gmail.com>
2022-03-03 11:30:26 +0100

    delete unused sequlize folders

--- codemods/sequelize4/__testfixtures__/user.controller.destructure.input.js
+++
@@ -1,6 +0,0 @@
-const db = require('./models');
-
-const { Op } = db.sequelize;
-const { Model } = db['sequelize'];
-const { findAndCount } = db.User;
-const { insertOrUpdate } = db['User'];
--- codemods/sequelize5/passed-as-parameter.js
+++
@@ -1,9 +0,0 @@
-const getRealKycStatus = (test, sequelize) => {
-  sequelize.find().spread();
-};
-const getRealKycStatus3 = function () {};
-function getRealKycStatus2(test) {}
-
-sequelize.define;
-kycService.getRealKycStatus(user, sequelize);
-getRealKycStatus(user, sequelize);
