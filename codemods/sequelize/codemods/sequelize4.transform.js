502553deda0328bf50d3ac582d8920b2fd96a730
Darko Pranjic <darkopranjic.4@gmail.com>
2022-03-03 17:18:31 +0100

    add sequelize.models handle case

--- codemods/sequelize/codemods/sequelize4.transform.js
+++ codemods/sequelize/codemods/sequelize4.transform.js
@@ -5,9 +5,20 @@ const {
   preparedRequireImportSearch,
   preparedSequelizeImportSearch,
 } = require("./helpers/ast");
+const {
+  SEQUELIZE_LOWER_CASE,
+  DEPRECATED_MODEL_METHODS,
+} = require("./helpers/constants");
 const { destructuringHelper } = require("./helpers/destructuring");
-const { directMethodUsageHelper } = require("./helpers/direct-usage");
-const { isDbImport, changeSequelizeCreationOperatorsAliasesProperty, isConfigAndHasOperatorAlias } = require("./helpers/sequelize");
+const {
+  directMethodUsageHelper,
+  searchForObjectProperties,
+} = require("./helpers/direct-usage");
+const {
+  isDbImport,
+  changeSequelizeCreationOperatorsAliasesProperty,
+  isConfigAndHasOperatorAlias,
+} = require("./helpers/sequelize");
 
 /**
  * TODO: Should we add hooks deprecated changes?
@@ -27,7 +38,10 @@ module.exports = (fileInfo, api, options) => {
     /**
      * Changing Sequelize initaialization file
      */
-    const sequelizeVariableDeclarator = root.find(j.VariableDeclarator, options.sequelizeImport || preparedSequelizeImportSearch());
+    const sequelizeVariableDeclarator = root.find(
+      j.VariableDeclarator,
+      options.sequelizeImport || preparedSequelizeImportSearch()
+    );
 
     // Grab the name of the imported sequelize module
     sequelizeVariableDeclarator.find(j.Identifier).forEach((node) => {
@@ -44,36 +58,58 @@ module.exports = (fileInfo, api, options) => {
     /**
      * Checking for the sequelize object files
      */
-    root.find(j.ObjectExpression, isConfigAndHasOperatorAlias).forEach((object) => {
-      const property = object.value.properties.find((property) => property.key.name === "operatorAliases");
+    root
+      .find(j.ObjectExpression, isConfigAndHasOperatorAlias)
+      .forEach((object) => {
+        const property = object.value.properties.find(
+          (property) => property.key.name === "operatorAliases"
+        );
 
-      if (property) {
-        if (property.value.value === true) {
-          property.value.value = 1;
-        } else if (property.value.value === false) {
-          property.value.value = 0;
+        if (property) {
+          if (property.value.value === true) {
+            property.value.value = 1;
+          } else if (property.value.value === false) {
+            property.value.value = 0;
+          }
         }
-      }
-    });
+      });
 
     /**
      * Working with created instance
      */
-    root.find(j.VariableDeclarator, preparedRequireImportSearch()).forEach((node) => {
-      if (isDbImport(node.value.init.arguments[0].value)) {
-        // Changing directly used deprecated Model methods and sequelize instance
-        root
-          // find all db
-          .find(j.MemberExpression, preparedObjectUsageSearch(node.value.id.name))
-          .forEach(directMethodUsageHelper);
+    root
+      .find(j.VariableDeclarator, preparedRequireImportSearch())
+      .forEach((node) => {
+        if (isDbImport(node.value.init.arguments[0].value)) {
+          // Changing directly used deprecated Model methods and sequelize instance
+          root
+            // find all db
+            .find(
+              j.MemberExpression,
+              preparedObjectUsageSearch(node.value.id.name)
+            )
+            .forEach(searchForObjectProperties);
 
-        // Solves Destructured variables const { Op } = db.sequelize
-        root
-          // Fetch all variables that are created with destructuring const { Op }  = ...
-          .find(j.VariableDeclarator, preparedDestructureObjectUsageSearch(node.value.id.name))
-          .forEach(destructuringHelper);
-      }
-    });
+          // Solves Destructured variables const { Op } = db.sequelize
+          root
+            // Fetch all variables that are created with destructuring const { Op }  = ...
+            .find(
+              j.VariableDeclarator,
+              preparedDestructureObjectUsageSearch(node.value.id.name)
+            )
+            .forEach(destructuringHelper);
+        }
+      });
+
+    // Find the sequelize.models and go through its every property
+    root
+      .find(
+        j.MemberExpression,
+        (obj) =>
+          obj.object.name === SEQUELIZE_LOWER_CASE &&
+          obj.property.name === "models"
+      )
+      .forEach(searchForObjectProperties);
   } catch (error) {
     console.log("error", error);
   }
