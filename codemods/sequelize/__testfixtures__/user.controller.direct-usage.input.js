230a64eefa8c836958edffbe2d5e98bc6534395a
Darko Pranjic <darkopranjic.4@gmail.com>
2022-03-03 17:18:11 +0100

    handle sequelize.models case

--- codemods/sequelize/__testfixtures__/user.controller.direct-usage.input.js
+++ codemods/sequelize/__testfixtures__/user.controller.direct-usage.input.js
@@ -1,10 +1,15 @@
-const db = require('./models');
+const db = require("./models");
 
-db['User'].findAndCount();
+db["User"].findAndCount();
 db.User.findAndCount();
+db.User.findAndCount().then();
 
 db.Sequelize.Op;
 db.sequelize.Op;
 
-const updateAttributes = db['User'].updateAttributes;
+const updateAttributes = db["User"].updateAttributes;
 const findById = db.User.findById;
+
+const passedSequelize = (sequelize) => {
+  sequelize.models.User.findAndCount();
+};
--- codemods/sequelize/__testfixtures__/user.controller.direct-usage.output.js
+++ codemods/sequelize/__testfixtures__/user.controller.direct-usage.output.js
@@ -1,10 +1,15 @@
-const db = require('./models');
+const db = require("./models");
 
-db['User'].findAndCountAll();
+db["User"].findAndCountAll();
 db.User.findAndCountAll();
+db.User.findAndCountAll().then();
 
 db.Sequelize.Op;
 db.Sequelize.Op;
 
-const updateAttributes = db['User'].update;
+const updateAttributes = db["User"].update;
 const findById = db.User.findByPk;
+
+const passedSequelize = (sequelize) => {
+  sequelize.models.User.findAndCountAll();
+};
